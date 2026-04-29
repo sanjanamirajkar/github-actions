7ce6e84ff8ace208b6317562999f05330002e9e0
Sanjana Mirajkar <sanjanamirajkar22@gmail.com>
2026-04-29 20:57:20 +0530

    updated a file

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 from flask import flask, render_template
 
+
 app = flask(__name__)
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+
 @app.route('/health')
 def health():
     return 'Server is up and running'
+
 
 if __name__ == "__main__":
     app.run(debug=True)
